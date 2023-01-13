f0f84abf46d3f9a7155d7cb3e8a04dc65584bf84
P S Samendh Krishna <95103156+MayMayGod@users.noreply.github.com>
2023-01-13 21:41:26 +0530

    Update Day 2.js

--- Day 2.js
+++ Day 2.js
@@ -2,14 +2,8 @@
 // get local machine date time
 const date = new Date();
 
-// get the date as a string
 const n = date.toDateString();
-
-// get the time as a string
 const time = date.toLocaleTimeString();
 
-// display date
 console.log('Date: ' + n);
-
-// display time
 console.log('Time: ' + time);
