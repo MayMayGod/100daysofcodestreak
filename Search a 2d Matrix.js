b18341b0753bfdb080d1241e026e4dcd0fd7225f
P S Samendh Krishna <95103156+MayMayGod@users.noreply.github.com>
2023-01-30 21:04:16 +0530

    Add files via upload

---
+++ Search a 2d Matrix.js
@@ -0,0 +1,18 @@
+var searchMatrix = function(matrix, target) {
+  var n = matrix.length;
+  var m = (matrix[0] || []).length;
+  var x = m - 1;
+  var y = 0;
+  var tmp = 0;
+  while (x >= 0 && y < n) {
+    tmp = matrix[y][x];
+    if (target === tmp) {
+      return true;
+    } else if (target > tmp) {
+      y++;
+    } else {
+      x--;
+    }
+  }
+  return false;
+};
